8be725f891401c8e5fb642aa8680bd073870d687
AlexBulavin <bulavin1970@icloud.com>
2021-06-27 12:49:13 +0300

    Add unit test file

---
+++ restapi/tests.py
@@ -0,0 +1,12 @@
+#from django.test import TestCase
+from unittest import TestCase
+
+# Create your tests here.
+# Предположм, что мы имеем для тестирования функцию, которая на вход получает два арнумента, а на выходе выдаёт их сумму
+def two_integers_sum(a, b):
+    return a+b
+
+class TestSum(TestCase):
+    def test_sum(self):
+        # Проверяем на конкретном примере, если результат выполнения функции two_integers_sum(1,2) равен 3
+        self.assertEqual(two_integers_sum(1, 2), 3)
